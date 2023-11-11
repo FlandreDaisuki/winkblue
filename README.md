6dcdd2063380268b3f23b7f6a783bdb69bf6e041
FlandreDaisuki <vbnm123c+github@gmail.com>
2023-11-11 18:17:49 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# winkblue
+
+An DOM element detector which API compatible to [sentinel-js](https://github.com/muicss/sentineljs)
+
+## why?
+
+1. sentinel-js can not find the `display: none;` elements.
+2. sentinel-js can not trigger multiple times if multiple selectors match the same element.
+
+## LICENSE
+
+[ISC](LICENSE)
---
+++ src/index.js
@@ -0,0 +1,56 @@
+// @ts-check
+
+const mem = Object.create(null);
+
+export default {
+  /**
+   * @param {string} selector
+   * @param {(el: Element) => void} cb
+   */
+  on: function winkblueOn(selector, cb) {
+    const wk = new WeakSet();
+    const mo = new MutationObserver(() => {
+      for (const el of Array.from(document.querySelectorAll(selector))) {
+        if (el && !wk.has(el)) {
+          wk.add(el);
+          return cb(el);
+        }
+      }
+    });
+    mo.observe(document.documentElement, { childList: true, subtree: true, attributes: true });
+
+    if (!mem[selector]) { mem[selector] = new Set(); }
+    mem[selector].add({ cb, mo });
+  },
+
+  /**
+   * @param {string} selector
+   * @param {(el: Element) => void} [cb]
+   */
+  off: function winkblueOff(selector, cb) {
+    const rSet = mem[selector];
+    if (!rSet) { return; }
+
+    if (cb) {
+      const found = Array.from(rSet).find((r) => r.cb === cb);
+      found.mo.disconnect();
+      rSet.delete(found);
+    }
+    else {
+      for (const r of rSet) {
+        r.mo.disconnect();
+        rSet.delete(r);
+      }
+    }
+  },
+
+  reset: function winkblueReset() {
+    for (const selector of Object.keys(mem)) {
+      const rSet = mem[selector];
+      for (const r of rSet) {
+        r.mo.disconnect();
+        rSet.delete(r);
+      }
+    }
+  },
+};
